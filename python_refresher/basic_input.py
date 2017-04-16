e6459699684598a642b5a25ec325886a6075af62
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-04-16 10:37:35 -0700

    fun alternative

--- python_refresher/basic_input.py
+++ python_refresher/basic_input.py
@@ -6,23 +6,25 @@ def who_do_you_know():
     string_list = input("Please give me a list of people you know: ")
     # split the string into a list
     names_list = string_list.split(",")
+    # convert to ensure spaces do not cause trouble
+    names_list_no_spaces = []
+    for name in names_list:
+        names_list_no_spaces.append(name.strip())
     # return that list
-    return names_list
+    return names_list_no_spaces
 
 
-def ask_user(known_ppl):
+def ask_user():
     # ask user for a name
     input_name = input("Please give me a name: ")
     # see if the name is in the list of people they know
-    for name in known_ppl:
-        if name == input_name:
-            print("Hey you know " + name + "!")
+    if input_name in who_do_you_know():
+        print("Hey you know " + input_name + "!")
 
 
 def main():
     """ Get list of names from user, and then ask user if a name is in the list."""
-    known_ppl = who_do_you_know()
-    ask_user(known_ppl)
+    ask_user()
 
 
 if __name__ == "__main__":
