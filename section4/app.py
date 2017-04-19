072f7b0acac64826c53a32e8b3b0057b56775ef1
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-04-19 07:48:48 -0700

    refactor code to use list comp vs filter

--- section4/app.py
+++ section4/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -8,16 +8,23 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': 'None'}, 404
+        item = [item for item in items if item['name'] == name]
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        if [item for item in items if item['name'] == name]:
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
-api.add_resource(Item, '/item/<string:name>')
+class ItemList(Resource):
+    def get(self):
+        return items
 
-app.run(port=5000)
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
