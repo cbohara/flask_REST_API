2ac1580145bc26c488737d0b5ddedb41c233bf86
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-04-19 19:36:13 -0700

    adding user authentication

--- section4/app.py
+++ section4/app.py
@@ -1,12 +1,20 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+
 
 app = Flask(__name__)
+app.secret_key = 'charlie'
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
+
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         item = [item for item in items if item['name'] == name]
         return {'item': item}, 200 if item else 404
@@ -20,11 +28,14 @@ class Item(Resource):
         items.append(item)
         return item, 201
 
+
 class ItemList(Resource):
     def get(self):
         return items
 
+
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+
 
 app.run(port=5000, debug=True)
