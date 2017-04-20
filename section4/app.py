ab474f74b367b15db6851ee5ca509d2051c72881
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-04-19 21:45:41 -0700

    completed section4

--- section4/app.py
+++ section4/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
@@ -14,6 +14,13 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank"
+    )
+
     @jwt_required()
     def get(self, name):
         item = [item for item in items if item['name'] == name]
@@ -24,7 +31,7 @@ class Item(Resource):
         if [item for item in items if item['name'] == name]:
             return {'message': "An item with name '{}' already exists".format(name)}, 400
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -39,7 +46,8 @@ class Item(Resource):
     def put(self, name):
         global items
 
-        data = request.get_json()
+        data = Item.parser.parse_args()
+
         item = [item for item in items if item['name'] == name]
 
         if not item:
