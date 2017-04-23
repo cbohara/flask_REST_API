54eccedead735501d9ca977b9e668d1b7789129d
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-04-23 11:55:19 -0700

    reading and writing items resource to the database

--- section5/app.py
+++ section5/app.py
@@ -1,70 +1,16 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
 from security import identity, authenticate
 from user import UserRegister
+from item import Item, ItemList
 
 
 app = Flask(__name__)
 app.secret_key = 'charlie'
 api = Api(app)
-
 jwt = JWT(app, authenticate, identity)
-
-items = []
-
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price',
-        type=float,
-        required=True,
-        help="This field cannot be left blank"
-    )
-
-    @jwt_required()
-    def get(self, name):
-        item = [item for item in items if item['name'] == name]
-        return {'item': item}, 200 if item else 404
-
-    @jwt_required()
-    def post(self, name):
-        if [item for item in items if item['name'] == name]:
-            return {'message': "An item with name " + name + " already exists"}, 400
-
-        data = Item.parser.parse_args()
-        item = {'name': name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-    @jwt_required()
-    def delete(self, name):
-        global items
-        items = [item for item in items if item['name'] != name]
-        return {'message': 'Item deleted'}
-
-    @jwt_required()
-    def put(self, name):
-        global items
-
-        data = Item.parser.parse_args()
-
-        item = [item for item in items if item['name'] == name]
-
-        if not item:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            items = [item for item in items if item['name'] != name]
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-
-        return item
-
-
-class ItemList(Resource):
-    def get(self):
-        return items
 
 
 api.add_resource(Item, '/item/<string:name>')
@@ -72,4 +18,5 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 
-app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
---
+++ section5/item.py
@@ -0,0 +1,79 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank"
+    )
+
+    @classmethod
+    def find_by_name(cls, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+
+        if row:
+            return {'item': {'name': row[0], 'price': row[1]}}
+
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {'message': 'Item not found'}, 404
+
+
+    @jwt_required()
+    def post(self, name):
+        if self.find_by_name(name):
+            return {'message': "An item with name '{}' already exists".format(name)}, 400
+
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = "INSERT INTO items VALUES (?, ?)"
+        cursor.execute(query, (item['name'], item['price']))
+        connection.commit()
+        connection.close()
+
+        return item, 201
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = [item for item in items if item['name'] != name]
+        return {'message': 'Item deleted'}
+
+    @jwt_required()
+    def put(self, name):
+        global items
+
+        data = Item.parser.parse_args()
+
+        item = [item for item in items if item['name'] == name]
+
+        if not item:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            items = [item for item in items if item['name'] != name]
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
