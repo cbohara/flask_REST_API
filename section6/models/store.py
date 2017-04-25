ed90b40e7a0d210b56161b863135e2541aeb7db8
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-04-25 07:50:34 -0700

    updating bug in store model

--- section6/models/store.py
+++ section6/models/store.py
@@ -9,7 +9,7 @@ class StoreModel(db.Model):
 
     items = db.relationship("ItemModel", lazy="dynamic")
 
-    def __init__(self, name, price):
+    def __init__(self, name):
         self.name = name
 
     def json(self):
