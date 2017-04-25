447b953d6c169cb4ab4be6fbe48917f0914fa635
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-04-24 20:51:07 -0700

    refactoring to use sqlalchemy

--- section6/resources/item.py
+++ section6/resources/item.py
@@ -1,4 +1,3 @@
-import sqlite3
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
@@ -60,15 +59,4 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect("data.db")
-        cursor = connection.cursor()
-        query = "SELECT * FROM items"
-        result = cursor.execute(query)
-
-        items = []
-        for row in result:
-            items.append({"name": row[0], "price": row[1]})
-
-        connection.close()
-
-        return {"items": items}
+        return {"items": [item.json() for item in ItemModel.query.all()]}
